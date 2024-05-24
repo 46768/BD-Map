7213fe0434a682b62a853bc6cbc874a30cc67354
theetat kittichai <46768@bodin.ac.th>
2024-05-24 18:33:19 +0700

    Inital Files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BD-Navigator
 Repository Containing The Source Code To The IS/PBL Project Of Bodin Interactive Map
+
+Webpage Under Construction, Check Back Later
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/BD-Navigator",
+    output: "export",
+    reactStrictMode: true,
+};
+
+export default nextConfig;
