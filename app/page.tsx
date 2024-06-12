8cc7b192b2f474c13056491165ee94259e992e1d
46768 <46768@bodin.ac.th>
2024-06-12 11:26:36 +0700

    Scraped Encoding, Embrace JSON

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,22 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import SideBtn from "./components/Sidebar/SideBtn";
 import SideLabel from "./components/Sidebar/SideLabel";
 
+/*
+interface FloorData {
+    [Surface: string]: {
+        x: number,
+        y: number,
+        sx: number,
+        sy: number,
+        r: number,
+        z: number,
+        desc: string,
+        id: string,
+        displayName: string[],
+        negate: boolean}
+}
+*/
+
 export default function Home() {
   return (
     <main className="pt-2 pl-4 fixed top-24 left-0 h-100vh-Navrem w-screen">
