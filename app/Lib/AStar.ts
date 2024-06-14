464e277971b4c54ff0fe9244a0b307c07d94606e
46768 <46768@bodin.ac.th>
2024-06-14 15:35:05 +0700

    Basic Optimization And Basic Visualization

--- app/Lib/AStar.ts
+++ app/Lib/AStar.ts
@@ -1,124 +1,130 @@
+import * as png from "pngjs"
+import * as fs from "fs"
+import * as path from "path"
+import Jimp from "jimp"
+
 export interface AStarCoordinate {
     x: number
     y: number
 }
 
+const ConvertString = (Coord: AStarCoordinate): string => `${Coord.x},${Coord.y}`
+
+const ConvertCoord = (Coord: string): AStarCoordinate => {
+    //console.log(Coord)
+    const SplitCoord: string[] = Coord.split(",")
+    return {
+        x: parseInt(SplitCoord[0]),
+        y: parseInt(SplitCoord[1]),
+    }
+}
+
+const Dist = (StartingNode: AStarCoordinate, EndingNode: AStarCoordinate): number =>
+    Math.floor(10 * (
+        Math.sqrt(((StartingNode.x - EndingNode.x) ** 2) +
+            ((StartingNode.y - EndingNode.y) ** 2))
+    ))
+
+function GetLowestValueIdx(Obj: { [s: string]: number }, Only: string[]) {
+    let ResultIdx: string = ""
+    let LowestVal: number = Infinity
+
+    Object.entries(Obj).forEach(([Idx, Val]) => {
+        if (Val < LowestVal && Only.indexOf(Idx) !== -1) {
+            ResultIdx = Idx
+            LowestVal = Val
+        }
+    })
+
+    return ResultIdx
+}
+
+function ReconstructPath(OriginMap: { [Node: string]: string }, Target: string) {
+    let Path: string[] = [Target]
+    let Current: string = Target
+    while (OriginMap[Current]) {
+        Current = OriginMap[Current]
+        Path.unshift(Current)
+    }
+
+    return Path
+}
+
 export default function AStar(Graph: Buffer, StartingNode: AStarCoordinate, TargetNode: AStarCoordinate) {
-    function ConvertString(Coord: AStarCoordinate): string {
-        return `${Coord.x},${Coord.y}`
-    }
-
-    function ConvertCoord(Coord: string): AStarCoordinate {
-        //console.log(Coord)
-        return {
-            x: parseInt(Coord.split(",")[0]),
-            y: parseInt(Coord.split(",")[1]),
-        }
-    }
-
-    function Dist(StartingNode: AStarCoordinate, EndingNode: AStarCoordinate): number {
-        return Math.floor(10*(Math.sqrt(((StartingNode.x-EndingNode.x)**2)+((StartingNode.y-EndingNode.y)**2))))
-    }
-
-    function GetLowestValueIdx(Obj: {[s:string]:number}, Only: string[]) {
-        let ResultIdx: string = ""
-        let LowestVal: number = Infinity
-
-        Object.entries(Obj).forEach(([Idx, Val]) => {
-            if (Val < LowestVal && Only.indexOf(Idx) !== -1) {
-                ResultIdx = Idx
-                LowestVal = Val
-            }
-        })
-
-        return ResultIdx
-    }
-
-    function ReconstructPath(OriginMap: {[Node: string]: string}, Target: string) {
-        let Path: string[] = [Target]
-        let Current: string = Target
-        while (OriginMap[Current]) {
-            Current = OriginMap[Current]
-            Path.unshift(Current)
-        }
-
-        return Path
-    }
-
-    Graph.forEach((Val, Idx) => console.log(`${Val} at ${Idx}`))
-
     const SX: number = Graph[0]
     const SY: number = Graph[1]
     const StartingStringNode: string = ConvertString(StartingNode)
+    const TargetStringNode: string = ConvertString(TargetNode)
 
-    let OpenArray: string[] = [ConvertString(StartingNode)]
+    let OpenArray: string[] = [StartingStringNode]
     let ClosedArray: Set<string> = new Set<string>()
-    let GCost: {[Node:string]: number} = {}
+    let GCost: { [Node: string]: number } = {}
     GCost[StartingStringNode] = 0
-    let FCost: {[Node:string]: number} = {}
+    let FCost: { [Node: string]: number } = {}
     FCost[StartingStringNode] = Dist(StartingNode, TargetNode)
-    let OriginNode: {[Node: string]: string} = {}
+    let OriginNode: { [Node: string]: string } = {}
 
-    let Limit: number = 0
-
-    while (OpenArray.length !== 0 && Limit <= 100000) {
+    while (OpenArray.length !== 0) {
         const CurrentNode = GetLowestValueIdx(FCost, OpenArray)
-        console.log(CurrentNode)
+        //console.log(CurrentNode)
 
         OpenArray = OpenArray.slice(0, OpenArray.indexOf(CurrentNode)).concat(
-            OpenArray.slice(OpenArray.indexOf(CurrentNode)+1)
+            OpenArray.slice(OpenArray.indexOf(CurrentNode) + 1)
         )
         ClosedArray.add(CurrentNode)
-        if (CurrentNode === ConvertString(TargetNode)) {
+        if (CurrentNode === TargetStringNode) {
             console.log("Target Reached")
-            return ReconstructPath(OriginNode, ConvertString(TargetNode))
+            return ReconstructPath(OriginNode, TargetStringNode)
         }
 
         const CurrentCoord: AStarCoordinate = ConvertCoord(CurrentNode)
+        const CX: number = CurrentCoord.x
+        const CY: number = CurrentCoord.y
         const Neighbor = [
-            `${CurrentCoord.x+1},${CurrentCoord.y}`,
-            `${CurrentCoord.x-1},${CurrentCoord.y}`,
-            `${CurrentCoord.x},${CurrentCoord.y+1}`,
-            `${CurrentCoord.x},${CurrentCoord.y-1}`,
-            `${CurrentCoord.x+1},${CurrentCoord.y+1}`,
-            `${CurrentCoord.x-1},${CurrentCoord.y+1}`,
-            `${CurrentCoord.x+1},${CurrentCoord.y-1}`,
-            `${CurrentCoord.x-1},${CurrentCoord.y-1}`,
+            `${CX + 1},${CY}`,
+            `${CX - 1},${CY}`,
+            `${CX},${CY + 1}`,
+            `${CX},${CY - 1}`,
+            `${CX + 1},${CY + 1}`,
+            `${CX - 1},${CY + 1}`,
+            `${CX + 1},${CY - 1}`,
+            `${CX - 1},${CY - 1}`,
         ]
-        Neighbor.forEach((Coord) => {
+        Neighbor.forEach((Coord, Idx) => {
             //console.log(Coord)
             if (ClosedArray.has(Coord)) return
             const NeighborCoord: AStarCoordinate = ConvertCoord(Coord)
-            if (NeighborCoord.x >= SX || NeighborCoord.y >= SY || NeighborCoord.x < 0 || NeighborCoord.y < 0) return
-            console.log(`Pos: ${NeighborCoord.x},${NeighborCoord.y} Is ${Graph[2+(NeighborCoord.x+(NeighborCoord.y*SX))]}`)
-            if (Graph[2+(NeighborCoord.x+(NeighborCoord.y*SX))] === 0) return
+            const NX: number = NeighborCoord.x
+            const NY: number = NeighborCoord.y
+            if (NX >= SX||
+                NY >= SY||
+                NX < 0||
+                NY < 0) return
+            if (Graph[2 + (NX + (NY * SX))] === 0) return
             const NeighborGCost: number = GCost[CurrentNode] +
-            Dist(CurrentCoord, NeighborCoord)
+                Idx > 3 ? 14 : 10
 
-            if (NeighborGCost < (GCost[Coord] ? GCost[Coord] : Infinity)) {
+            if (NeighborGCost < (GCost[Coord] ?? Infinity)) {
                 OriginNode[Coord] = CurrentNode
                 GCost[Coord] = NeighborGCost
                 FCost[Coord] = NeighborGCost + Dist(NeighborCoord, TargetNode)
                 if (OpenArray.indexOf(Coord) === -1) {
                     OpenArray.push(Coord)
-                } 
+                }
             }
         })
-        //console.log(Limit)
-        Limit++
     }
 
     return false
 }
 
-const TestBuffer = Buffer.alloc(27)
+const DX = 50
+const DY = 50
+const TestBuffer = Buffer.alloc(2+(DX*DY))
 
-TestBuffer.write(String.fromCharCode(5, 5, 
-    1, 1, 1, 1, 1,
-    1, 0, 0, 1, 1,
-    1, 1, 0, 1, 1,
-    1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1,
-))
+TestBuffer.write(String.fromCharCode(DX, DY, ...new Array(DX*DY).fill(0).map(() => Math.random() < 0.75 ? 1 : 0)))
 
-console.log(AStar(TestBuffer, {x:0, y:2}, {x:4, y:2}))
+let IMG = new Jimp(DX, DY)
+IMG
+
+console.log(AStar(TestBuffer, { x: 0, y: 0 }, { x: DX-1, y: DY-1 }))
