93bb8b98f3ee06e1c4333c2dfcabf41b8e136d48
theetat kittichai <46768@bodin.ac.th>
2024-07-04 19:46:08 +0700

    Binary heap

--- lib-rust/src/lib.rs
+++ lib-rust/src/lib.rs
@@ -1,34 +1,17 @@
+pub mod binary_heap;
 mod utils;
-mod binary_heap;
 
+use binary_heap::MinBinaryHeap;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct AStarCoordinate(pub i32, pub i32);
 
-fn dist(p0: AStarCoordinate, p1: AStarCoordinate) -> u32 {
-    p0.0.abs_diff(p1.0) + p0.1.abs_diff(p1.1)
-}
-
-fn get_smallest_value_idx(
-    v: &HashMap<AStarCoordinate, u32>,
-    filter: &Vec<AStarCoordinate>,
-) -> AStarCoordinate {
-    let mut res_idx: AStarCoordinate = AStarCoordinate(0, 0);
-    let search_filter = 0;
-    for (&idx, &val) in v.iter() {
-        if val < v[&res_idx] && {
-            match filter.get(1) {
-                Some(_) => true,
-                None => false,
-            }
-        } {
-            res_idx = idx;
-        }
-    }
-    res_idx
+fn dist(p0: AStarCoordinate, p1: AStarCoordinate) -> i32 {
+    p0.0.abs_diff(p1.0) as i32 + p0.1.abs_diff(p1.1) as i32
 }
 
 fn reconstruct_path(
@@ -60,33 +43,91 @@ pub fn a_star(
     start_node: AStarCoordinate,
     target_node: AStarCoordinate,
 ) -> Vec<AStarCoordinate> {
-    const ITERATOR: [u8; 8] = [
+    const ITERATOR: [i32; 8] = [
         0b0100, 0b1100, 0b0001, 0b0011, 0b0101, 0b1111, 0b0111, 0b1101,
     ];
+    const UNDEFINED: (AStarCoordinate, i32) = (AStarCoordinate(0, 0), 2147483647);
+
+    fn bubble_up_fn(l_idx: (AStarCoordinate, i32), p_idx: (AStarCoordinate, i32)) -> bool {
+        l_idx.1 < p_idx.1
+    }
+
+    fn bubble_down_fn(
+        c_idx: (AStarCoordinate, i32),
+        c1_idx: Option<&(AStarCoordinate, i32)>,
+        c2_idx: Option<&(AStarCoordinate, i32)>,
+    ) -> (bool, bool) {
+        let c1_val = match c1_idx {
+            Some(&val) => val,
+            None => UNDEFINED,
+        };
+        let c2_val = match c2_idx {
+            Some(&val) => val,
+            None => UNDEFINED,
+        };
+
+        (
+            c_idx.1 > c1_val.1 || c_idx.1 > c2_val.1,
+            if c1_val.1 > c2_val.1 { true } else { false },
+        )
+    }
+
+    fn handle_option(inp: Option<(AStarCoordinate, i32)>) -> (AStarCoordinate, i32) {
+        match inp {
+            Some(val) => val,
+            None => UNDEFINED,
+        }
+    }
+
     let sx: i32 = graph[0];
     let sy: i32 = graph[1];
 
-    let mut open_node: Vec<AStarCoordinate> = vec![start_node];
-    let mut closed_node: Vec<AStarCoordinate> = vec![];
-    let mut g_cost: HashMap<AStarCoordinate, u32> = HashMap::new();
+    let mut closed_node: HashSet<AStarCoordinate> = HashSet::new();
+    let mut g_cost: HashMap<AStarCoordinate, i32> = HashMap::new();
     g_cost.insert(start_node, 0);
-    let mut f_cost: HashMap<AStarCoordinate, u32> = HashMap::new();
-    f_cost.insert(start_node, dist(start_node, target_node));
+    let mut f_cost: MinBinaryHeap<(AStarCoordinate, i32)> = MinBinaryHeap::new();
+    f_cost.insert((start_node, dist(start_node, target_node)), &bubble_up_fn);
     let origin: HashMap<AStarCoordinate, AStarCoordinate> = HashMap::new();
 
-    let mut loop_limit: u32 = 0;
+    let mut loop_limit: i32 = 0;
 
     'main: loop {
-        if open_node.len() == 0 && loop_limit >= 10000 {
+        if f_cost.get_size() == 0 && loop_limit >= 10000 {
             break 'main;
         }
 
-        let current_node: AStarCoordinate = get_smallest_value_idx(&f_cost, &open_node);
-        open_node.retain(|&x| x != current_node);
-        closed_node.push(current_node);
+        let extracted_val = handle_option(f_cost.extract(&bubble_down_fn));
+        let current_node: AStarCoordinate = extracted_val.0;
+        closed_node.insert(current_node);
 
         if target_node == current_node {
             break 'main;
+        }
+
+        'neighbor_search: for &neighbor in ITERATOR.iter() {
+            let n_x = current_node.0
+                + ((neighbor & 0b0100)
+                    >> 2 * if ((neighbor & 0b1000) >> 3) == 0 {
+                        1
+                    } else {
+                        -1
+                    });
+            let n_y = current_node.0
+                + ((neighbor & 0b0001)
+                    * if ((neighbor & 0b0010) >> 1) == 0 {
+                        1
+                    } else {
+                        -1
+                    });
+            let neighbor_coord = AStarCoordinate(n_x, n_y);
+            let graph_idx = graph[(2+n_x+(n_y*sx)) as usize];
+            if closed_node.contains(&neighbor_coord) || n_x >= sx || n_y >= sy || n_x < 0 || n_y < 0 || graph_idx == 0 {
+                continue 'neighbor_search;
+            }
+            let neighbor_g_cost = match g_cost.get(&neighbor_coord) {
+                Some(&current_g_cost) => current_g_cost+dist(neighbor_coord, target_node),
+                None => dist(start_node, neighbor_coord)+dist(neighbor_coord, target_node),
+            };
         }
 
         loop_limit += 1;
