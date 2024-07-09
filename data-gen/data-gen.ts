54de1d5a8f7fc30fd8f49f98b36657bbe46c1750
theetat kittichai <46768@bodin.ac.th>
2024-07-09 23:11:45 +0700

    Interfaces

--- data-gen/data-gen.ts
+++ data-gen/data-gen.ts
@@ -1,3 +1,4 @@
+import { JSAStarCoordinate } from "@/app/lib/aStar/astar"
 import * as fs from "fs"
 import * as path from "path"
 
@@ -12,10 +13,81 @@ ADAT: (8+(SX*SY))
     32BIT SY
     SX*SY BIT 2DDAT
 
-DDAT: (6(2POS:2SIZE:1NEGATEBOOL:1LYR)*SCOUNT)
-    POSX,POSY,SX,SY,NEGATEBOOL,LYR\n
+DDAT: (9(1ID:2POS:2SIZE:1LYR:3RGB)*SCOUNT)
+    ID,POSX,POSY,SX,SY,LYR,RGB_R,RGB_G,RGB_B\n
 
-TRANSLSTION: (6(2APOS:2DPOS:1LYR)*(ASX*ASY))
-    APOSX,APOSY,DPOSX,DPOSY,LYR,\n
+TRANSLATION: (5(2APOS:2DPOS:1LYR)*(ASX*ASY))
+    APOSX,APOSY,DPOSX,DPOSY,LYR\n
+
+METADATA: (5(1ID:1DESC:1[NAME_ALIAS]:1ADAT_LOCAT:1LYR)*SCOUNT)
+    ID, DESCRIPTION, ARRAY<STRING>(NAME_ALIAS), ADAT_LOCATION, LYR\n
+
+//Input Data For Parsing, Apply To All Below
+
+INPT: (MARKER_COUNT, ID, [MARKER_DATA])[]
+    MARKER_DATA {
+        LAT:number
+        LON:number
+    }
+
+//FlowChart
+
+INPT + USR_INPT(METADATA) => (ADAT, DDAT, TRANSLATION, METADATA).zip (PGK.zip)
+PGK.zip => Github Pull Request + Approval => DATA Addition/Change
 */
 
+//Input GPS Data
+interface MarkerData {
+    Latitude: number
+    Longitude: number
+}
+
+interface InputDataSection {
+    MarkerCount: number
+    ID: string
+    Markers: MarkerData[]
+}
+
+type InputData = InputDataSection[]
+
+//Input Metadata
+interface MetadataSection {
+    ID: string
+    Desc: string
+    NameAlias: string[]
+    AStarLocation: JSAStarCoordinate
+    Layer: number
+}
+
+type Metadata = MetadataSection[]
+
+//Output DDAT
+interface SurfaceData {
+    PosX: number
+    PosY: number
+    SizeX: number
+    SizeY: number
+    Layer: number
+    RGB_Red: number
+    RGB_Green: number
+    RGB_Blue: number
+}
+
+//Output TRANSLATION
+interface TranslationLine {
+    ADATPosX: number
+    ADATPosY: number
+    DDATPosX: number
+    DDATPosY: number
+    Layer: number
+}
+
+//Parser
+
+//Parser Input/Variable
+
+//ADAT Parser
+
+//DDAT Parser
+
+//TRANSLATION Parser
