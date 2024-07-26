f6a11dbb72afb0e638811a561ba929a7a506ce80
46768 <46768@bodin.ac.th>
2024-07-26 15:41:54 +0700

    Parsing cont

--- data-gen/data-gen.ts
+++ data-gen/data-gen.ts
@@ -2,7 +2,7 @@ import { JSAStarCoordinate } from "@/app/lib/aStar/astar";
 
 export type SurfaceData = number[];
 
-export function parseDrawData(InptGPS: number[]) {
+export function parseDrawData(InptGPS: number[]): SurfaceData[] {
   let returnData: SurfaceData[] = [];
   let currentSection: number = 0;
   let markerCount: number = InptGPS[currentSection];
@@ -12,8 +12,17 @@ export function parseDrawData(InptGPS: number[]) {
   while (nextSection < InptGPS.length) {
      returnData.push(slicedSection)
   }
+  return returnData;
 }
 
-export function parseMainBus(MainBusPoint: number[][]) {
-  
+export function parseMainBus(MainBusPoint: number[][]): {
+  graph: number[]
+  translation: number[][]
+} {
+   let graph: number[] = [15, Math.ceil(MainBusPoint.length / 4), ...Array(15*Math.ceil(MainBusPoint.length / 4)).fill(0)]
+   let translation: number[][] = []
+
+   MainBusPoint.forEach((point, idx) => {
+    graph[] 
+   });
 }
