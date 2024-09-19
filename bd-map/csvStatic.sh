06f649b47ad79ecc878ffb14a2d74060cc423c76
46768 <4676@bodin.ac.th>
2024-09-19 14:05:48 +0000

    fixed build script, works properly now

--- bd-map/csvStatic.sh
+++ bd-map/csvStatic.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 cd public
-mkdir data
+pwd
 
-graphCsv = $(ls ./map.data | grep -E 'v[0-9]+\.graph\.csv' | sort -V | tail -n 1)
-graphicCsv = $(ls ./map.data | grep -E 'v[0-9]+\.graphic\.csv' | sort -V | tail -n 1)
-translationCsv = $(ls ./map.data | grep -E 'v[0-9]+\.translation\.csv' | sort -V | tail -n 1)
+if [ ! -d ./data ]; then
+	mkdir data
+fi
 
-mv $graphCsv ./data && mv ./data/$graphCsv ./data/graph.csv
-mv $graphicCsv ./data && mv ./data/$graphicCsv ./data/graphic.csv
-mv $translationCsv ./data && mv ./data/$translationCsv ./data/translation.csv
+graphCsv="$(ls ./map.data | grep -E 'v[0-9]+\.graph\.csv' | sort -V | tail -n 1)"
+graphicCsv="$(ls ./map.data | grep -E 'v[0-9]+\.graphic\.csv' | sort -V | tail -n 1)"
+translationCsv="$(ls ./map.data | grep -E 'v[0-9]+\.translation\.csv' | sort -V | tail -n 1)"
+
+echo $graphCsv
+echo $graphicCsv
+echo $translationCsv
+
+cp ./map.data/$graphCsv ./data && mv ./data/$graphCsv ./data/graph.csv
+cp ./map.data/$graphicCsv ./data && mv ./data/$graphicCsv ./data/graphic.csv
+cp ./map.data/$translationCsv ./data && mv ./data/$translationCsv ./data/translation.csv
--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 cd bd-map
+pwd
 chmod +x csvStatic.sh
 ./csvStatic.sh && npm run build-only
 
 if [ -d ./dist ]; then
+	if [ -d ../dist ]; then
+		rm -rf ../dist
+	fi
 	mv ./dist ..
 fi
