09cd5def5b87cd94733275c2b5fe159b38d88f29
46768 <46768@bodin.ac.th>
2024-09-20 04:12:42 +0700

    gitignore data dir, map dragging

--- bd-map/src/components/Map.vue
+++ bd-map/src/components/Map.vue
@@ -1,57 +1,85 @@
 <script setup lang="ts">
-	import { ref, useTemplateRef, onMounted, defineProps } from 'vue'
+	import { ref, useTemplateRef, onMounted } from 'vue'
 	import { graphData } from '../stores/graphData'
 	import { translationTable } from '../stores/translationTable'
 	const canvas = useTemplateRef('canvas-ref')
-	const props = defineProps({
-		source: Number,
-		target: Number,
-		currentLoc: [Number, Number],
-		currentLayer: Number,
-	})
+	const props = defineProps<{
+		source: number,
+		target: number,
+		currentLoc: [number, number],
+		currentLayer: number,
+	}>()
 
 	const translation = translationTable()
 	const graph = graphData()
 	const polygon = ref<number[][]>([])
 
+	const canvasCtx = ref<CanvasRenderingContext2D | null>(null)
+
+	const x = ref<number>(window.innerWidth/2)
+	const y = ref<number>(window.innerHeight/2)
+	const dragging = ref<boolean>(false)
+
+	async function fetchData(file: string, callback: Function) {
+		fetch(`/BD-Map/data/${file}`)
+		.then(response => response.text())
+		.then(text => {callback(text)})
+		.catch(error => console.error(`Failed fetching ${file}: `, error))
+	}
+
 	function drawScreen(canvasCtx: CanvasRenderingContext2D, polygonData: number[][], currentLayer: number) {
-		for (let polygonDat of polygonData.filter(dat => dat[8] == currentLayer)) {
-			canvasCtx.beginPath()
-			canvasCtx.strokeStyle = `rgb(${polygonDat[9]}, ${polygonDat[10]}, ${polygonDat[11]})`
-			canvasCtx.moveTo(polygonDat[0], polygonDat[1])
-			canvasCtx.lineTo(polygonDat[2], polygonDat[3])
-			canvasCtx.lineTo(polygonDat[4], polygonDat[5])
-			canvasCtx.lineTo(polygonDat[6], polygonDat[7])
+		canvasCtx.canvas.height = window.innerHeight
+		canvasCtx.canvas.width = window.innerWidth
+		canvasCtx.fillStyle = "rgb(0, 204, 102)"
+		canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+		for (let polygonDat of polygonData) {
+			canvasCtx.fillStyle = `rgb(${polygonDat[5]}, ${polygonDat[6]}, ${polygonDat[7]})`
+			canvasCtx.fillRect(polygonDat[0]+x.value, polygonDat[1]+y.value, polygonDat[2], polygonDat[3])
+		}
+	}
+
+	function mouseBtnHandler(evt: MouseEvent) {
+		dragging.value = !dragging.value
+	}
+	
+	function mouseMoveHandler(evt: MouseEvent) {
+		if (dragging.value) {
+			x.value += evt.movementX
+			y.value += evt.movementY
+			if (canvasCtx.value) drawScreen(canvasCtx.value, polygon.value, props.currentLayer)
+		}
+	}
+
+	function adjustScreen() {
+		if (canvasCtx.value) {
+			drawScreen(canvasCtx.value, polygon.value, props.currentLayer)	
 		}
 	}
 
 	onMounted(() => {
-		fetch('/data/graph.csv')
-		.then(response => response.text())
-		.then(data => {graph.defineGraph(data)})
-		.catch(error => {console.error("error fetching graph.csv: ", error)})
-
-		fetch('/data/translation.csv')
-		.then(response => response.text())
-		.then(data => {translation.defineAlias(data)})
-		.catch(error => {console.error("error fetching translation.csv: ", error)})
-
-		fetch('/data/graphic.csv')
-		.then(response => response.text())
-		.then(data => {
-			polygon.value = data.split('\n').map(dataBlock => dataBlock.split(',').map(val => parseInt(val)))
-		})
-		.catch(error => {console.error("error fetching graphic.csv: ", error)})
-
+		window.addEventListener('resize', adjustScreen)
 		if (canvas.value && canvas.value.getContext) {
 			const ctx = canvas.value.getContext("2d")
 			if (ctx) {
-				drawScreen(ctx, polygon.value, props.currentLayer as number)			
+				canvasCtx.value = ctx
 			}
 		}
+
+		fetchData("graph.csv", graph.defineGraph)
+		fetchData("translation.csv", translation.defineAlias)
+		fetchData("graphic.csv", (data: string) => {
+			polygon.value = data.split('\n').map(dataBlock => dataBlock.split(',').map(val => parseInt(val)))
+			if (canvasCtx.value) {
+				drawScreen(canvasCtx.value, polygon.value, props.currentLayer)	
+			}
+		})
 	})
 </script>
 
 <template>
-	<canvas ref="canvas-ref" />
+	<canvas
+		@mousedown="mouseBtnHandler"
+		@mouseup="mouseBtnHandler"
+		@mousemove="mouseMoveHandler"
+		ref="canvas-ref"></canvas>
 </template>
--- bd-map/src/views/HomeView.vue
+++ bd-map/src/views/HomeView.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-	<Map class="fixed top-0 left-0"/>
+	<Map :source="0" :target="0" :current-loc="[0,0]" :current-layer="0" class="fixed top-0 left-0"/>
 </template>
