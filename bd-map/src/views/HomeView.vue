b449250decdcd17a227b28bc05a7d82c3068d8f2
46768 <4676@bodin.ac.th>
2024-10-15 14:56:00 +0000

    deleted old frontend directory

--- bd-map/src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-	import Map from '../components/Map.vue'
-</script>
-
-<template>
-	<Map class="fixed top-0 left-0"/>
-</template>
--- bd-map/src/views/MapDev.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang='ts'>
-</script>
-
-<template>
-	<p>this is a map editor page</p>
-</template>
