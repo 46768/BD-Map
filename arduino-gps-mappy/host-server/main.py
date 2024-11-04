1420c37ec7710ceb9803413621910c5fbaf2a8eb
46768 <4676@bodin.ac.th>
2024-11-04 08:57:28 +0000

    mappy host file renaming and ui refactoring

--- arduino-gps-mappy/host-server/main.py
+++ arduino-gps-mappy/host-server/main.py
@@ -1,9 +1,4 @@
-import serial
 import platform
-import threading
-from SerialHandler import SerialHandler
-from PolygonHandler import PolygonHandler
-from interface import makeApp
 
 
 port = '/dev/ttyUSB0'  # 'dev/ttyUSB0' on linux
@@ -13,25 +8,3 @@ elif platform.system() == "Windows":
     port = 'COM3'
 
 baudRate = 19200
-serialPort
-polygonHandler
-
-
-def serialInterface():
-    global port
-    global baudRate
-    global serialPort
-    global polygonHandler
-    try:
-        serialPort = SerialHandler(baudRate=baudRate, port=port)
-        polygonHandler = PolygonHandler()
-    except serial.SerialException as e:
-        print(f"Error: {e}")
-        return
-
-
-running = True
-serialThread = threading.Thread(target=serialInterface, daemon=True)
-serialThread.start()
-app = makeApp(serialPort, polygonHandler)
-app.run()
