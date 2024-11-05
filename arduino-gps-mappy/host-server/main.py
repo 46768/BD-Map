71b31f08b5a45f5f9318cb4a4283e0c073e1d273
46768 <4676@bodin.ac.th>
2024-11-05 01:59:45 +0000

    changed from tkinter to cli

--- arduino-gps-mappy/host-server/main.py
+++ arduino-gps-mappy/host-server/main.py
@@ -1,4 +1,5 @@
 import platform
+import argparse
 
 
 port = '/dev/ttyUSB0'  # 'dev/ttyUSB0' on linux
@@ -8,3 +9,11 @@ elif platform.system() == "Windows":
     port = 'COM3'
 
 baudRate = 19200
+print("Hekki Wolrd")
+
+parser = argparse.ArgumentParser(
+        prog="Mappy CLI",
+        description="Command line tool for mappy the gps module",
+        epilog="2024/2567 PBL/IS"
+    )
+parser.add_argument("-n", "--new")
